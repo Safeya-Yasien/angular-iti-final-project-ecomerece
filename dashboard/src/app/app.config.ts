9c9d4a4348fa68411747a1f1a757bc31d42c46e7
Safeya-Yasien <ssafeya5@gmail.com>
2026-05-07 10:30:16 +0300

    update angular.json, app.config.ts and vercel.json

--- dashboard/src/app/app.config.ts
+++ dashboard/src/app/app.config.ts
@@ -1,11 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; // 1. استيراد الدالة
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient() // 2. إضافة الخدمة هنا
-  ]
+  providers: [provideRouter(routes), provideHttpClient()],
 };
