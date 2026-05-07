01ae212a49b17d8ccb39d9bac1bffdf9ad92e05a
Asalla Eliwa <asallaeliwa06@gmail.com>
2026-05-07 09:02:32 +0300

    feat: implement user order history and admin status updates

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProductListComponent } from './components/admin/product-list/product-list.component';
 import { AddProductComponent } from './components/admin/product-form/product-form.component';
-
+import { OrderManagementComponent } from './components/order-management/order-management';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // أول ما يفتح يروح للـ login
   { path: 'login', component: LoginComponent },
   { path: 'products', component: ProductListComponent},
-  { path: 'add-product', component:AddProductComponent }
+  { path: 'add-product', component:AddProductComponent },
+  { path: 'orders', component: OrderManagementComponent }
 ];
