22459a0dc799782a8710af1b1125793e8eacbd24
Safeya-Yasien <ssafeya5@gmail.com>
2026-05-07 13:01:24 +0300

    update api url and remove console log

--- dashboard/src/app/login/login.component.ts
+++ dashboard/src/app/login/login.component.ts
@@ -1,57 +1,55 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { jwtDecode } from 'jwt-decode';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  apiUrl = `${environment.apiUrl}/auth/login`;
   loginForm: FormGroup;
   errorMessage: string = '';
 
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onLogin() {
     if (this.loginForm.valid) {
       const loginData = this.loginForm.value;
-      
-      // تأكدي من مسار الـ API الخاص بكِ
-      this.http.post<any>('http://localhost:3000/api/auth/login', loginData).subscribe({
-        next: (res) => {
-          console.log('Login Response:', res);
-          
-          if (res.token) {
-  
-            localStorage.setItem('token', res.token);
-            
-            try {
-       
-              const decoded: any = jwtDecode(res.token);
-              console.log('Decoded Payload:', decoded);
 
- 
-              const userRole = decoded.role 
-     console.log(userRole);
-     
-              
+      this.http.post<any>(this.apiUrl, loginData).subscribe({
+        next: (res) => {
+          if (res.token) {
+            localStorage.setItem('token', res.token);
+
+            try {
+              const decoded: any = jwtDecode(res.token);
+
+              const userRole = decoded.role;
+
               if (userRole) {
-                localStorage.setItem("role", userRole);
+                localStorage.setItem('role', userRole);
 
                 if (userRole === 'admin') {
                   this.router.navigate(['/admin/products']);
@@ -59,23 +57,23 @@ export class LoginComponent {
                   this.router.navigate(['/']);
                 }
               } else {
-                this.errorMessage = 'لم يتم العثور على صلاحيات لهذا المستخدم';
+                this.errorMessage = 'No user role found';
               }
-
             } catch (decodeError) {
               console.error('Error decoding token:', decodeError);
-              this.errorMessage = 'حدث خطأ في قراءة بيانات الدخول';
+              this.errorMessage = 'Error decoding token';
             }
           }
         },
         error: (err) => {
           console.error('Login Error:', err);
-          // تخصيص رسالة الخطأ بناءً على رد السيرفر
-          this.errorMessage = err.error?.message || 'فشل تسجيل الدخول، تأكدي من البيانات أو اتصال السيرفر';
-        }
+          this.errorMessage =
+            err.error?.message ||
+            'Failed to log in, please check your credentials or server';
+        },
       });
     } else {
-      this.errorMessage = 'يرجى إدخال البيانات بشكل صحيح';
+      this.errorMessage = 'Please enter the correct data';
     }
   }
 }
