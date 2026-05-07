3971b075a1d8764a108fa04b883b96ca757ae586
Safeya-Yasien <ssafeya5@gmail.com>
2026-05-07 09:53:13 +0300

    solve deploy problems

--- dashboard/src/app/components/admin/edit-product/edit-product.component.ts
+++ dashboard/src/app/components/admin/edit-product/edit-product.component.ts
@@ -1,40 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router'; // استيراد RouterLink
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../../services/product.service';
 import { CommonModule } from '@angular/common';
-import Swal from 'sweetalert2'; 
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-edit-product',
   standalone: true,
 
-  imports: [ReactiveFormsModule, CommonModule, RouterLink], 
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './edit-product.component.html',
-  styleUrls: ['./edit-product.component.css']
+  styleUrls: ['./edit-product.component.css'],
 })
 export class EditProductComponent implements OnInit {
-    selectedFile: File | null = null;
+  selectedFile: File | null = null;
   editForm!: FormGroup;
   productId!: string;
-  categories: any[] = []; 
+  categories: any[] = [];
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    public router: Router, 
-    private productSer: ProductService
+    public router: Router,
+    private productSer: ProductService,
   ) {}
 
   ngOnInit(): void {
     this.initForm();
     this.productId = this.route.snapshot.params['id'];
-        this.loadCategories();
+    this.loadCategories();
     this.loadProduct();
-       
   }
 
-
-   onFileSelect(event: any) {
+  onFileSelect(event: any) {
     const file = event.target.files[0];
     if (file) {
       this.selectedFile = file;
@@ -47,27 +50,21 @@ export class EditProductComponent implements OnInit {
       price: [0, [Validators.required, Validators.min(1)]],
       quantity: [0, [Validators.required, Validators.min(1)]],
       imageCover: ['', Validators.required],
-      category: ['', Validators.required]
+      category: ['', Validators.required],
     });
   }
-loadProduct() {
-  this.productSer.getProductById(this.productId).subscribe({
-    next: (res) => {
-   
-      
-      this.editForm.get('title')?.setValue(res.data.title);
-      this.editForm.get('price')?.setValue(res.data.price);
-      this.editForm.get('description')?.setValue(res.data.description);
-      this.editForm.get('quantity')?.setValue(res.data.quantity);
-      this.editForm.get('imageCover')?.setValue(res.data.imageCover);
-      this.editForm.get('category')?.setValue(res.data.category._id);
-      
-      
-    }
-  });
-}
-
-
+  loadProduct() {
+    this.productSer.getProductById(this.productId).subscribe({
+      next: (res) => {
+        this.editForm.get('title')?.setValue(res.data.title);
+        this.editForm.get('price')?.setValue(res.data.price);
+        this.editForm.get('description')?.setValue(res.data.description);
+        this.editForm.get('quantity')?.setValue(res.data.quantity);
+        this.editForm.get('imageCover')?.setValue(res.data.imageCover);
+        this.editForm.get('category')?.setValue(res.data.category._id);
+      },
+    });
+  }
 
   loadCategories() {
     this.productSer.getAllCategories().subscribe({
@@ -77,63 +74,55 @@ loadProduct() {
       },
       error: (err) => {
         console.error('Failed to load categories', err);
-      }
+      },
     });
   }
   // loadProduct() {
   //   this.productSer.getProductById(this.productId).subscribe({
   //     next: (res) => {
- 
-  //       this.editForm.patchValue(res.data || res); 
+
+  //       this.editForm.patchValue(res.data || res);
   //     },
   //     error: (err) => console.error(err)
   //   });
   // }
 
-
-
-onUpdate() {
-
- if (this.editForm.valid) {
+  onUpdate() {
+    if (this.editForm.valid) {
       const formData = new FormData();
 
-      // إضافة البيانات للـ FormData
       formData.append('title', this.editForm.get('title')?.value);
       formData.append('description', this.editForm.get('description')?.value);
       formData.append('price', this.editForm.get('price')?.value);
       formData.append('quantity', this.editForm.get('quantity')?.value);
       formData.append('category', this.editForm.get('category')?.value);
       if (this.selectedFile) {
-          formData.append('imageCover', this.selectedFile);
+        formData.append('imageCover', this.selectedFile);
       }
-    
 
-
-    this.productSer.updateProduct(this.productId, formData).subscribe({
-      next: () => {
-        Swal.fire({
-        title: 'Updated!',
+      this.productSer.updateProduct(this.productId, formData).subscribe({
+        next: () => {
+          Swal.fire({
+            title: 'Updated!',
             text: 'Product updated successfully',
             icon: 'success',
             timer: 2000,
-            showConfirmButton: false
-        });
-        this.router.navigate(['/admin/products']);
-      },
-      error: (err) => {
-        Swal.fire({
-          title: 'Error',
+            showConfirmButton: false,
+          });
+          this.router.navigate(['/admin/products']);
+        },
+        error: (err) => {
+          Swal.fire({
+            title: 'Error',
             text: 'Update failed, please try again',
-            icon: 'error'
-
-        });
-      }
-    });
+            icon: 'error',
+          });
+        },
+      });
+    }
   }
-}
- 
+
   goBack() {
     this.router.navigate(['/products']);
   }
 }
-
